e372f2c27d8499335601b7144ad5b866f44e0454
SireneNouira <sirene.nouira16@gmail.com>
2025-02-09 18:10:57 +0100

    formulaire dinscription et connexion fonctinne mais a rajouter les redirection after et styles

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
+    {
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            // $email = (new TemplatedEmail())
+            //     ->from('t7b4o@example.com')
+            //     ->to($user->getEmail())
+            //     ->subject('Votre compte a bien été créé')
+            //     ->text('Merci pour votre inscription !')
+            //     ->htmlTemplate('emails/confirmation_email.html.twig')
+            //     ->context([
+            //         'data' => $user,
+            //     ]);
+
+            //     $mailer->send($email);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Compte crée avec succès.');
+
+            return $security->login($user, 'form_login', 'main');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
